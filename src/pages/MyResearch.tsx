a3c805cdc8e9fbd5197710fc0314d9e25b03a042
spectrecoder <pong96111kind@gmail.com>
2024-09-17 23:04:45 +0900

    feat: add failed action

--- src/pages/MyResearch.tsx
+++ src/pages/MyResearch.tsx
@@ -13,11 +13,17 @@ function MyResearch() {
   const { connectionState } = useSelector((store: any) => store.user);
   const [loading, setLoading] = useState(false);
   const [nftData, setNftData] = useState<null | Array<Certificate>>(null)
+  const [err, setErr] = useState(false);
 
   const getResearch = async () => {
-    let res = await getResearchByUser(kit, connectionState.publicKey);
-    setLoading(false)
-    setNftData(res);
+    try {
+      setErr(false);
+      let res = await getResearchByUser(kit, connectionState.publicKey);
+      setLoading(false)
+      setNftData(res);
+    } catch (error) {
+      setErr(true)
+    }
   }
 
   useEffect(() => {
@@ -55,13 +61,34 @@ function MyResearch() {
     )
   }
 
+  const FailIcon = () => {
+    return (
+      < svg
+        fill="red"
+        viewBox="0 -8 528 528"
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-12 w-12" >
+        <title>fail</title>
+        <path d="M264 456Q210 456 164 429 118 402 91 356 64 310 64 256 64 202 91 156 118 110 164 83 210 56 264 56 318 56 364 83 410 110 437 156 464 202 464 256 464 310 437 356 410 402 364 429 318 456 264 456ZM264 288L328 352 360 320 296 256 360 192 328 160 264 224 200 160 168 192 232 256 168 320 200 352 264 288Z" />
+      </svg >
+    )
+  }
+
   return (
     <div className="flex flex-col items-center h-screen w-screen overflow-x-hidden px-4">
       <Header />
-      <div className="mx-auto max-w-screen-2xl py-12">
-        {loading ? <div className="flex gap-2 items-center text-xl">
-          <LoadingIcon />
-          Loading...
+      <div className="mx-auto max-w-screen-2xl py-12 h-[calc(100%-300px)]">
+        {loading ? <div className="flex gap-2 items-center text-xl h-36">
+          {!err ?
+            <div>
+              <LoadingIcon />
+              Loading...
+            </div> :
+            <div className="flex gap-2 items-center justify-center">
+              <FailIcon />
+              Failed for loading research data
+            </div>
+          }
         </div> :
           nftData?.map((_, idx) => (
             <div key={idx} className="z-20 grid grid-cols-1 gap-5 md:grid-cols-5 mt-4">
